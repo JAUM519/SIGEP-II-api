17fe98a030f9c65e90e7e8780d0789c33b5ad931
Juan Esteban Vila Marín <jvilamarin31@gmail.com>
2026-05-15 10:09:54 -0500

    Backend mas robusto en seguridad.

--- backend/src/main/java/com/apirest/backend/dtos/requests/usuarios/LoginRequest.java
+++ backend/src/main/java/com/apirest/backend/dtos/requests/usuarios/LoginRequest.java
@@ -3,6 +3,7 @@ package com.apirest.backend.dtos.requests.usuarios;
 import com.apirest.backend.models.enums.TipoIdentificacionUsuarios;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,6 @@ public class LoginRequest {
     private TipoIdentificacionUsuarios tipoIdentificacion;
     @NotBlank
     private String numeroIdentificacion;
+    @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres")
     private String contraseña;
 }
--- backend/src/main/java/com/apirest/backend/dtos/requests/usuarios/NuevoUsuarioRequest.java
+++ backend/src/main/java/com/apirest/backend/dtos/requests/usuarios/NuevoUsuarioRequest.java
@@ -3,6 +3,8 @@ package com.apirest.backend.dtos.requests.usuarios;
 import com.apirest.backend.models.enums.TipoIdentificacionUsuarios;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +20,8 @@ public class NuevoUsuarioRequest {
     private String numeroIdentificacion;
     @Email
     private String email;
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres")
+    @Pattern(regexp = "^[a-zA-Z0-9@#*!]+$", message = "Caracteres no permitidos en la contraseña")
     private String contraseña;
 }
